b6e47a7e40c624c7c6147ddb253b316d0a55a6b5
Joe Morris <joem@joe-morriss-macbook-pro.local>
2014-05-01 16:12:53 -0400

    Initial Commit

---
+++ mathUtility/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  mathUtility
+//
+//  Created by Joe Morris on 5/1/14.
+//  Copyright (c) 2014 joemdesign, llc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
